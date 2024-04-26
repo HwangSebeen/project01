0197aee2d8d4c49930c9ea89cbdf0176243e8dfe
HwangSebeen <hsbsbpro427@gmail.com>
2024-04-26 22:47:11 +0900

    2024-04-26 로그인 구현 진행중33

--- src/main/java/com/project/login/service/LoginService.java
+++ src/main/java/com/project/login/service/LoginService.java
@@ -3,7 +3,7 @@ package com.project.login.service;
 import java.util.List;
 import java.util.Map;
 
-public interface LoginService {
+public interface LoginService { 
 
 	// 로그인
 	Map<String, Object> login(Map<String, Object> inputMap);
